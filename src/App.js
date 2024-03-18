dceb6000fe9794450e355d788f0a80d68111954b
tubi55 <tubi55@nate.com>
2024-03-19 02:44:46 +0900

    기본 tailwind 설치 및 설정 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className="text-4xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
 
 export default App;
+
+//tailwindcss 설치 및 config파일 초기화
+//npm install -D tailwindcss postcss autoprefixer
+//npx tailwindcss init
