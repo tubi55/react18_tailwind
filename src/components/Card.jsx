f055db6af430616ef7b2a584b25fcf5d4dce7a77
tubi55 <tubi55@nate.com>
2024-03-20 02:08:48 +0900

    flex에서 자식 요소의 박스 크기 유지시키기

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = ({ data }) => {
   return (
-    <article className="w-[calc((100%-(40px*4))/5)] mb-15 border-b border-slate-400">
-      <figure className="w-full mb-4">
+    <article className="w-[calc((100%-(40px*4))/5)] mb-15 border-b border-slate-400 self-start pb-10">
+      <figure className="w-full mb-2">
         <img
           className="w-full"
           src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./variable.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
