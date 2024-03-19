234184299747cba32336f112008752cff15828ba
tubi55 <tubi55@nate.com>
2024-03-20 02:35:46 +0900

    profile영역 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const fetchFlickr = async () => {
-  const num = 20;
+  const num = 40;
   const flickr_api_key = process.env.REACT_APP_FLICKR_API;
   const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api_key}&per_page=${num}&format=json&nojsoncallback=1&method=`;
   const method_interest = "flickr.interestingness.getList";
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,29 @@
 const Card = ({ data }) => {
   return (
-    <article className="w-[calc((100%-(40px*4))/5)] mb-15 border-b border-slate-400 self-start pb-10">
-      <figure className="w-full mb-2">
+    <article className="w-[calc((100%-(40px*4))/5)] mb-18 border-b border-slate-300 self-start pb-3">
+      <figure className="w-full mb-2 cursor-pointer">
         <img
           className="w-full"
           src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
           alt={data.title}
         />
       </figure>
-      <h3>{data.title}</h3>
+      <h3 className="mb-6 text-md text-slate-800">{data.title}</h3>
+
+      <div className="flex items-center w-full gap-2 text-xs text-orange-400 cursor-pointer">
+        <img
+          className="w-10"
+          src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+          alt="profile"
+          onError={(e) =>
+            e.target.setAttribute(
+              "src",
+              "https://www.flickr.com/images/buddyicon.gif"
+            )
+          }
+        />
+        <span>{data.owner}</span>
+      </div>
     </article>
   );
 };
