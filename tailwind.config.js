3e976ef1fcc113acc31dd1ce90a57e8f32eff9ce
tubi55 <tubi55@nate.com>
2024-03-20 19:56:35 +0900

    max-width로 미디어쿼리 breakpoint 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        wide: { max: "1400px" },
+        web: { max: "1280px" },
+        tb: { max: "1024px" },
+        mb: { max: "640px" },
+      },
+    },
   },
   plugins: [],
 };
