a8acb3135e2ee2771cb26bb9d6a4238ac4a7d678
tubi55 <tubi55@nate.com>
2024-03-20 19:54:50 +0900

    scss변수를 활용하여 컬럼갯수 반응형 처리

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,7 +7,7 @@ const List = () => {
   const { data, isPending, isError, error } = useFlickrQuery(FlickrType);
 
   return (
-    <section className="relative flex flex-wrap justify-start w-full gap-5">
+    <section className="relative flex flex-wrap justify-start w-full gap-[var(--gap)]">
       {isPending && <p className="infoText">Loading Flickr Data...</p>}
       {isError && <p className="infoText">{error?.message}</p>}
       {!isPending && data?.length === 0 && (
